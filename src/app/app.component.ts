9747d3799a0086c6ae50564d6078b232be2d9969
jissmon <jisshub>
2020-10-01 14:01:51 +0530

    project reactive forms assignment - forsms synced

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'reactive-form-project-ver2';
+  projectDetailsForm: FormGroup;
+  statusArray: string[] = ['stable', 'critical', 'finished'];
+
+  ngOnInit() {
+    // initialize project details form,
+    this.projectDetailsForm = new FormGroup({
+      projectData: new FormGroup({
+        name: new FormControl(null),
+        email: new FormControl(null),
+        status: new FormControl(this.statusArray[0]),
+      }),
+    });
+  }
+  onSubmit() {
+    console.log(this.projectDetailsForm);
+  }
 }
