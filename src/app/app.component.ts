f035159f899056fef6171fef11315556f0a01db9
jissmon <jisshub>
2020-10-01 18:14:56 +0530

    forms - reactive approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +16,27 @@ export class AppComponent {
     // initialize project details form,
     this.projectDetailsForm = new FormGroup({
       projectData: new FormGroup({
-        name: new FormControl(null),
+        name: new FormControl(null, this.forbiddenName),
         email: new FormControl(null),
         status: new FormControl(this.statusArray[0]),
       }),
     });
   }
+  // form submit
   onSubmit() {
-    console.log(this.projectDetailsForm);
+    console.log(this.projectDetailsForm.get('projectData.name'));
+  }
+  // validate the project name, not allow name 'Test'
+  forbiddenName(arg: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (arg.value === 'Test') {
+          resolve({ nameIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
